6bd2c8d41a489b68c6fc18a0ba05037315bc409b
unknown <mikimaku69@gmail.com>
2023-08-25 14:14:46 +0600

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Switch, Route, Routes} from 'react-router-dom';
 import './App.css';
 import app from './firebase.init';
+import Signup from '../src/Authentication/signup';
+import Login from './Authentication/Login';
 
 function App() {
   return (
     <div>
-     
+   <Routes>
+    <Route path='/signup' element={<Signup></Signup>}></Route>
+    <Route path='/login' element={<Login></Login>}></Route>
+    <Route></Route>
+
+   </Routes>
     </div>
   );
 }
--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Button, Card, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-const signup = () => {
+const Login = () => {
     return (
-          <Card className="shadow-lg p-3 mb-5 bg-white rounded w-50 mx-auto" style={{ width: '15rem',height:'30rem',marginTop:'70px' }}>
+        <Card className="shadow-lg p-3 mb-5 bg-white rounded w-50 mx-auto" style={{ width: '10rem',height:'30rem',marginTop:'70px' }}>
             <Card.Body className=''>
             <div >
-        <h2 className="text-center mt-3 mb-3">Please Register</h2>
+        <h2 className="text-center mt-3 mb-3">Please Login</h2>
         <Form>
           <Form.Group className="mb-3 mt-4" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
@@ -31,34 +31,22 @@ const signup = () => {
               required
             />
           </Form.Group>
-          <input
-            type="checkbox"
-            name="terms"
-            id="terms"
-          ></input>
-          <label
-            htmlFor="terms"
-          >
-            Accept Terms and Condition
-          </label>
             <Button
               className="black w-50  mx-auto d-block mb-4 mt-4"
               variant="dark"
             >
-              Sign Up
+              Login
             </Button>
         </Form>
         <p>
-          Already have an account?
-          <Link to='/login' className='danger'>
-            Please Login
+          Forget Password? 
+          <Link> click here
           </Link>
         </p>
       </div>
             </Card.Body>
           </Card>
-        );
-      
+    );
 };
 
-export default signup;
+export default Login;
