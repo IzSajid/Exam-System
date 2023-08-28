066715f3c979f1dcd33db7285dd9c90c2f338cec
unknown <mikimaku69@gmail.com>
2023-08-28 15:29:56 +0600

    8th commit

--- src/Home/Features/Feature.js
+++ src/Home/Features/Feature.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import './Feature.css';
 
-const Feature = ({feature}) => {
-    const {_id,name,image,description}=feature;
+const Feature = ({ feature }) => {
+    const { _id, name, image, description } = feature;
+
+    const cardStyle = {
+        backgroundImage: `url(${image})`, // Set background image using template string
+    };
+
     return (
         <div>
-             <div className=" feature card w-96  shadow-xl chat-image">
-  <figure><img src={image} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">{name}!</h2>
-    <p>{description}</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary"></button>
-    </div>
-  </div>
-</div>
+            <div className="card-container  card w-96 chat-image mt-2 mb-5">
+                <div className="card-body shadow-xl bg-black rounded border-white" style={cardStyle}>
+                    <h2 className="card-title">{name}!</h2>
+                    <p>{description}</p>
+                    <div className="card-actions justify-end">
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
