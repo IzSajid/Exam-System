0f5a3bf5d3a5724d245a95d3b0afeda1398f83d5
unknown <mikimaku69@gmail.com>
2023-10-25 23:13:30 +0600

    delete button

--- src/Cohorts/ShowStudents.js
+++ src/Cohorts/ShowStudents.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './ShowStudents.css'
+import { Button } from 'react-bootstrap';
 
 
 
@@ -20,15 +21,17 @@ const ShowStudents = () => {
   
         {
             students.map(student=>
-                <li className=' shadow-lg shadow-indigo-500/50 border h-16 text-center font-semibold my-auto'>
-               
-        {student.email}
-        
-        <div class="relative w-11 h-6 overflow-hidden  rounded-full ml-40 mb-3">
+                <li className=' shadow-lg shadow-indigo-500/50 border h-16 text-center font-semibold grid grid-cols-3'>
+                    <div class="relative w-11 h-6 overflow-hidden  rounded-full ml-40 mt-2">
             <svg class="relative w-11 h-6 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                 
                 <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
         </div>
+               
+        {student.email}
+        <Button className='btn btn-sm btn-circle mt-3' variant='white'>x</Button>
+        
+        
         </li>
        
        )
--- src/Cohorts/Students.js
+++ src/Cohorts/Students.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 // import { Button, Form } from 'react-bootstrap';
 import ShowStudents from './ShowStudents';
 import './Students.css'
+import { Button } from 'react-bootstrap';
 
 const Students = () => {
   const [recipientEmail, setRecipientEmail] = useState('');
@@ -22,34 +23,34 @@ const Students = () => {
     
           if (response.ok) {
             setMessage('Invitation sent successfully!');
-          } else {
+
+            // for auto reload the page after successfully adding an email 
+
+            setTimeout(() => {
+              window.location.reload();
+            }, 2000);
+
+          } 
+          
+          else {
             setMessage('Failed to send invitation. Please try again.');
           }
-        } catch (error) {
+        } 
+        catch (error) {
           setMessage('An error occurred. Please try again later.');
         }
       };
-  //       const email=e.target.email.value;
-  //       const addStudent={email};
-  //       fetch('http://localhost:5000/student',{
-  //           method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(addStudent),
-  // })
-  // .then(res=>res.json())
-  // .then(data=>console.log(data))
+  
     
    
     return (
       
         
         <div>
-      <h1>Email Invitation Form</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="recipientEmail">Recipient's Email:</label>
-        <input
+      <h1 className='text-center mt-4'>Email Invitation Form</h1>
+      <form style={{textAlign:'center'}} onSubmit={handleSubmit}>
+        <label  htmlFor="recipientEmail">Recipient's Email:</label>
+        <input style={{marginLeft:'15px',marginBottom:'15px',marginTop:'15px'}}
           type="email"
           id="recipientEmail"
           name="recipientEmail"
@@ -57,9 +58,11 @@ const Students = () => {
           onChange={(e) => setRecipientEmail(e.target.value)}
           required
         />
-        <button type="submit">Send Invitation</button>
+        <Button className="black w-25  mx-auto d-block mb-4 mt-4" variant="primary" type='submit'>
+                Send Invitation
+        </Button>
       </form>
-      <div>{message}</div>
+      <div className='text-center mb-4 text-red-700 font-bold'>{message}</div>
       <ShowStudents></ShowStudents>
     </div>
         
